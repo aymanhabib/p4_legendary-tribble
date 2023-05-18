e3fe87096975b74e25dd79fc9f45cdbf17f6155a
eric sohel <90656205+esohel30@users.noreply.github.com>
2023-05-18 13:33:21 -0400

    Update apimethods.py

--- app/apimethods.py
+++ app/apimethods.py
@@ -2,3 +2,9 @@ import requests
 import base64
 import json
 import urllib
+
+# Spotify API
+SPOTIFY_API_BASE_URL = 'https://api.spotify.com/v1'
+SPOTIFY_CLIENT_ID = '7e8682088ce94ba4b6599a96fa11129b'
+SPOTIFY_CLIENT_SECRET = 'e68b36407b4a4cb9ad14c3ea756076fd'
+
