3c4f897e624752d352024bd996f2b3c09d873178
eric sohel <90656205+esohel30@users.noreply.github.com>
2023-05-18 13:33:37 -0400

    Update apimethods.py

--- app/apimethods.py
+++ app/apimethods.py
@@ -8,3 +8,25 @@ SPOTIFY_API_BASE_URL = 'https://api.spotify.com/v1'
 SPOTIFY_CLIENT_ID = '7e8682088ce94ba4b6599a96fa11129b'
 SPOTIFY_CLIENT_SECRET = 'e68b36407b4a4cb9ad14c3ea756076fd'
 
+# YouTube Data API
+YOUTUBE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3/'
+YOUTUBE_API_KEY = 'AIzaSyD4Xwq5Dxz8dHs6ULSzoULdO9meYnN9F2Q'
+
+# Get Spotify access token
+def get_token():
+    auth_string = SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+
+    response = requests.post(url, headers=headers, data=data)
+    json_result = response.json()
+
+    token = json_result["access_token"]
+    return token
