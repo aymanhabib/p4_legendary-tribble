8ac16c49cbde5d455cb794ad216d35ce7e16a5bd
JH Li <jhli@JHs-MacBook-Air.fios-router.home>
2023-05-24 00:02:15 -0400

    last

--- app/__init__.py
+++ app/__init__.py
@@ -45,7 +45,7 @@ def authenticate():
     pw = request.form['password']
     if login(user,pw):
         session['username'] = request.form['username']
-        return render_template('home.html')
+        return render_template('home.html', user = session['username'])
     else:
         return render_template('login.html', errorTextL = "Authentication Failed")
     return redirect('/')
